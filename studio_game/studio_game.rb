f76241c7774f27577dcbbfe000a79807810f208d
Steve Roma <sjroma@hotmail.com>
2017-03-03 18:13:55 -0500

    chap20: initial commit of Inheritance code to studioGame...still need to do the Bonus Round

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,5 +1,7 @@
 require_relative 'player'
 require_relative 'game'
+require_relative 'clumsy_player'
+require_relative 'berserk_player'
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
@@ -7,6 +9,12 @@ player3 = Player.new("curly", 125)
 
 knuckleheads = Game.new("Knuckleheads")
 knuckleheads.load_players(ARGV.shift || "players.csv")
+
+klutz = ClumsyPlayer.new("klutz", 105)
+knuckleheads.add_player(klutz)
+
+berserker = BerserkPlayer.new("berserker", 50)
+knuckleheads.add_player(berserker)
 
 #knuckleheads.add_player(player1)
 #knuckleheads.add_player(player2)
